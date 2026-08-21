4018c0a9c1db02bf2b7e672ee728a049e60dcdbb
anirudh senani <55765373+Anirudh-Senani@users.noreply.github.com>
2026-08-21 22:26:38 +0530

    Step 20: build_policy_value_net

--- model.py
+++ model.py
@@ -233,8 +233,31 @@ def init_value_head(hidden_channels=16):
 
     return model
 
-# Step 20 - build_policy_value_net (not yet solved)
-# TODO: implement
+# Step 20 - build_policy_value_net
+import torch
+import torch.nn as nn
+
+
+class PolicyValueNet(nn.Module):
+    def __init__(self, in_channels, hidden_channels, num_columns):
+        super().__init__()
+    
+        self.backbone = init_conv_backbone(in_channels, hidden_channels)
+        self.policy_head = init_policy_head(hidden_channels, num_columns)
+        self.value_head = init_value_head(hidden_channels)
+
+
+    def forward(self, x):
+        x = self.backbone(x)
+        logits = self.policy_head(x)
+        value = self.value_head(x)
+        return (logits, value)
+
+
+def build_policy_value_net(in_channels=2, hidden_channels=16, num_columns=7):
+    """Compose backbone + policy head + value head into one nn.Module."""
+    # TODO: build an nn.Module with backbone, policy_head, value_head attributes
+    return PolicyValueNet(in_channels, hidden_channels, num_columns)
 
 # Step 21 - policy_value_forward (not yet solved)
 # TODO: implement
