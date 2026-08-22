d7eee0c211a0edb3e9005c19455fb590ed4e5d94
anirudh senani <55765373+Anirudh-Senani@users.noreply.github.com>
2026-08-22 15:13:07 +0530

    Step 36: run_mcts

--- model.py
+++ model.py
@@ -426,8 +426,17 @@ def run_one_simulation(root, net, c_puct):
 
     backup_value(leaf, value)
 
-# Step 36 - run_mcts (not yet solved)
-# TODO: implement
+# Step 36 - run_mcts
+def run_mcts(state, to_play, net, num_simulations, c_puct):
+    # TODO: build a fresh root for (state, to_play) and run num_simulations PUCT simulations
+    root = make_mcts_node()
+    root['board'] = state
+    root['to_play'] = to_play
+
+    for _ in range(num_simulations):
+        run_one_simulation(root, net, c_puct)
+
+    return root
 
 # Step 37 - visit_count_policy (not yet solved)
 # TODO: implement
